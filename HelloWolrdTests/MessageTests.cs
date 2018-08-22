bcbd56c96dca2fd74dd05f23a46ecd1898e503f9
EwenIllirian9138 <ewen.billot@orange.fr>
2018-08-22 14:43:06 +0200

    Mock fonctionnel

--- HelloWolrdTests/MessageTests.cs
+++ HelloWolrdTests/MessageTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HelloWolrdTests.Fake;
+using NMock;
 
 namespace HelloWorld.Tests
 {
@@ -46,6 +47,53 @@ namespace HelloWorld.Tests
             FakeTime fake = new FakeTime(new DateTime(2018, 08, 19, 21, 45, 00));
 
             Message message = new Message(fake);
+            string result = message.PropertyMDate;
+            Assert.IsTrue(result.Contains("tu es en week"));
+        }
+    
+
+
+        [TestMethod()]
+        public void MessageTest_WithMock_matin()
+        {
+            MockFactory mockFactory = new MockFactory();
+            Mock<ITime> mockITime = mockFactory.CreateMock<ITime>();
+            mockITime.Expects.Any.GetProperty(_ => _.date).WillReturn(new DateTime(2018, 08, 21, 11, 00, 00));
+
+            Message message = new Message(mockITime.MockObject);
+            string result = message.PropertyMDate;
+            Assert.IsTrue(result.Contains("Bonjour"));
+        }
+        [TestMethod()]
+        public void MessageTest_WithMock_aprem()
+        {
+            MockFactory mockFactory = new MockFactory();
+            Mock<ITime> mockITime = mockFactory.CreateMock<ITime>();
+            mockITime.Expects.Any.GetProperty(_ => _.date).WillReturn(new DateTime(2018, 08, 21, 14, 10, 00));
+
+            Message message = new Message(mockITime.MockObject);
+            string result = message.PropertyMDate;
+            Assert.IsTrue(result.Contains("Bon après-midi"));
+        }
+        [TestMethod()]
+        public void MessageTest_WithMock_soir()
+        {
+            MockFactory mockFactory = new MockFactory();
+            Mock<ITime> mockITime = mockFactory.CreateMock<ITime>();
+            mockITime.Expects.Any.GetProperty(_ => _.date).WillReturn(new DateTime(2018, 08, 21, 19, 25, 00));
+
+            Message message = new Message(mockITime.MockObject);
+            string result = message.PropertyMDate;
+            Assert.IsTrue(result.Contains("Bonsoir"));
+        }
+        [TestMethod()]
+        public void MessageTest_WithMock_weekend()
+        {
+            MockFactory mockFactory = new MockFactory();
+            Mock<ITime> mockITime = mockFactory.CreateMock<ITime>();
+            mockITime.Expects.Any.GetProperty(_ => _.date).WillReturn(new DateTime(2018, 08, 19, 21, 45, 00));
+
+            Message message = new Message(mockITime.MockObject);
             string result = message.PropertyMDate;
             Assert.IsTrue(result.Contains("tu es en week"));
         }
